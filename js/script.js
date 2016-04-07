f23a47109b37928f2e8e0f4b1cf5fc11df3820ff
Jack Keating <keatjm@gmail.com>
2016-04-07 15:00:31 -0500

    Stop/Start/Reset

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 $(function() {
     var clock = $('.minclock').FlipClock({
-    // ... options ? ? ? no
+        "autoStart": false
+    });
+
+    $("#start").on("click", function() {
+      clock.start();
+      $("#start").prop("disabled", true);
+      $("#stop").prop("disabled", false);
+      $("#reset").prop("disabled", false);
+    });
+    $("#stop").on("click", function() {
+      clock.stop();
+      $("#start").prop("disabled", false);
+      $("#stop").prop("disabled", true);
+      $("#reset").prop("disabled", false);
+    });
+    $("#reset").on("click", function() {
+      clock.setTime(0);
+      clock.stop();
+      $("#start").prop("disabled", false);
+      $("#stop").prop("disabled", true);
+      $("#reset").prop("disabled", true);
     });
 });
